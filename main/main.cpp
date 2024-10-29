4366d5408ca406b5aebec2ab655002e072a0c841
영준 <74802526+chldudwns@users.noreply.github.com>
2024-10-29 20:10:12 +0900

    랜덤 추첨 구현중

--- main/main.cpp
+++ main/main.cpp
@@ -8,15 +8,32 @@ HANDLE hConsoleOut;
 CONSOLE_CURSOR_INFO curCursorInfo;
 
 void setInitSetting();
-
 void setPosition(int x, int y);
-
 void drawOutline();
 
+void resetDeck();
+void drawRandomCard();
+
+int flagSPADE[13];
+int flagCLOVER[13];
+int flagHEART[13];
+int flagDIAMOND[13];
+
+int countSPADE;
+int countCLOVER;
+int countHEART;
+int countDIAMOND;
+
+typedef struct trump
+{
+
+} trump;
 int main()
 {
 	setInitSetting();
 	drawOutline();
+
+	resetDeck();
 
 	while (1)
 	{
@@ -24,8 +41,6 @@ int main()
 	}
 
 	setPosition(11, 0);
-
-	printf("test2222222");
 
 	return 0;
 }
@@ -36,14 +51,13 @@ void setInitSetting()
 	GetConsoleCursorInfo(hConsoleOut, &curCursorInfo);
 	curCursorInfo.bVisible = 0;
 	SetConsoleCursorInfo(hConsoleOut, &curCursorInfo);
+	srand(time(0));
 }
-
 void setPosition(int y, int x)
 {
 	COORD pos = { x, y };
 	SetConsoleCursorPosition(hConsoleOut, pos);
 }
-
 void drawOutline()
 {
 	setPosition(0, 0);
@@ -69,4 +83,31 @@ void drawOutline()
 		printf("─");
 	}
 	
+}
+
+void resetDeck()
+{
+	for (int i = 0; i < 52; i++)
+	{
+		flagSPADE[i] = 1;
+		flagCLOVER[i] = 1;
+		flagHEART[i] = 1;
+		flagDIAMOND[i] = 1;
+	}
+	countSPADE = 13;
+	countCLOVER = 13;
+	countHEART = 13;
+	countDIAMOND = 13;
+}
+void drawRandomCard()
+{
+	int r = (float)rand() / RAND_MAX * countSPADE;
+	for (int i = 0; i < r; i++)
+	{
+		if (flagSPADE[i] = 0)
+		{
+			i--;
+			continue;
+		}
+	}
 }
